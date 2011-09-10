63215b8578038189fa9f189e126ace475de594e3
Cakez0r <lewis.ellington@gmail.com>
2011-09-10 18:37:50 +0100

    Added an example of how SQL commands can be ran when the value on a result changes.

--- DynamicORM_Examples/Program.cs
+++ DynamicORM_Examples/Program.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace DynamicORM_Examples
 {
     class Program
     {
+        //Update this to use your connection string.
+        static DynamicORM.DynamicORM db = new DynamicORM.DynamicORM("Data Source=localhost\\SQLExpress; Initial Catalog=DynamicORM; User Id=DynamicORM; Password=password123;");
+
         static void Main(string[] args)
         {
             //Deploy script for example database can be found in SQL\TestCreate.sql
-
-            //Update this to use your connection string.
-            DynamicORM.DynamicORM db = new DynamicORM.DynamicORM("Data Source=localhost\\SQLExpress; Initial Catalog=DynamicORM; User Id=DynamicORM; Password=password123;");
 
             //Clear the table via an SQL command
             db.Command("DELETE FROM People");
@@ -33,6 +34,20 @@ namespace DynamicORM_Examples
             {
                 Console.WriteLine("ID: {0} - Name: {1} - LastLogin: {2}", person.PersonID, person.Name.TrimEnd(), person.LastLogin);
             }
+
+            //Fire an event when a property changes on a person
+            //This is a little convoluted, but works because the dynamic is actually an ExpandoObject
+            dynamic dave = db.Command("SELECT TOP 1 * FROM People WHERE [Name] = 'Dave'").First();
+            ((INotifyPropertyChanged)dave).PropertyChanged += (dynamic sender, PropertyChangedEventArgs e) =>
+                {
+                    IDictionary<string, object> daveProperties = (IDictionary<string, object>)dave;
+                    db.Command(string.Format("UPDATE People SET {0} = '{1}'", e.PropertyName, daveProperties[e.PropertyName]));
+                };
+
+            //Update dave's last login, which will cause the value to update in the database;
+            dave.LastLogin = DateTime.Now - TimeSpan.FromDays(1);
+
+            Console.WriteLine("Done! Press any key to exit.");
 
             Console.ReadKey();
         }
